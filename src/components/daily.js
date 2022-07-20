3f47acb0aa8cdfa0d9ed6779a7534f808ba2bc14
Kim sanggyung <ksang0906@gmail.com>
2022-07-20 16:00:37 +0900

    날짜 처리 방식 변경

--- src/components/daily.js
+++ src/components/daily.js
@@ -1,47 +1,32 @@
 import React, { useState } from 'react';
 
 function Daily(){
-  const date = new Date(2022,1,19);
-  let [month, setMonth] = useState(date.getMonth())
-  let [year, setYear] = useState(date.getFullYear())
-  let [day, setDay] = useState(date.getDate())
-  let thisMonthlast = new Date(year, month+1, 0).getDate();
-  let beforeMonthLast = new Date(year, month, 0).getDate();
-  
+  const [date, setDate] = useState(new Date());
 
   const nextDay = () => {
-    console.log(thisMonthlast)
-    if (day >= thisMonthlast) {
-      setMonth(month+1)
-      return setDay(day=1)
+    const adddDate = (date) => {
+      var result = new Date(date);
+      result.setDate(result.getDate() + 1);
+      return result;
     }
-    if(month >= 12){
-      console.log(day)
-      setYear(year+1)
-      return setMonth(month = 0);
-    }
-    return setDay(day+1)
+    return setDate(adddDate(date))
   }
 
   const prevDay = () => {
-    console.log(beforeMonthLast)
-    if (day <= 1) {
-      setMonth(month-1)
-      return setDay(beforeMonthLast);
+    const minusDate = (date) => {
+      var result = new Date(date);
+      result.setDate(result.getDate() - 1);
+      return result;
     }
-    if(month <= 0){
-      setYear(year-1)
-      return setMonth(month = 11);
-    }
-    return setDay(day-1);
+    return setDate(minusDate(date))
   }
   return(
     <div>
       <div className="w-full h-32 bg-blue-400 mt-24 p-6 font-jua">
-        <div className="font-jua flex item-center flex justify-center text-xl float-none">{year}년</div>
+        <div className="font-jua flex item-center flex justify-center text-xl float-none">{date.getFullYear()}년</div>
         <div className="font-jua flex item-center flex justify-center text-xl mt-2">
           <button onClick={prevDay} className="h-6 w-16 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold rounded-l">어제</button>
-          <span className="text-xl ml-4 mr-4 font-bolds">{month+1}월{day}일</span>
+          <span className="text-xl ml-4 mr-4 font-bolds">{date.getMonth()+1}월{date.getDate()}일</span>
           <button onClick={nextDay} className="h-6 w-16 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold rounded-r">내일</button>
         </div>
       </div>
