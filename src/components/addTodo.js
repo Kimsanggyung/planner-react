e6028de5a2e1bd6bfffd3b33f01e10365188bf4b
Kim sanggyung <ksang0906@gmail.com>
2022-08-01 17:47:59 +0900

    indexedDB에 일정추가 컴포넌트

--- src/components/addTodo.js
+++ src/components/addTodo.js
@@ -1,45 +1,94 @@
 import AddTodoError from "../parts/addTodoError";
+import { setItem } from '../context/indexed'
+import { useState } from "react";
 
-function AddTodo(){
-  let todo = '';
-  let details = '';
-  let error = '';
+function AddTodo({loggedUser}){
+  let getDate = new Date();
+  let year = getDate.getFullYear();
+  let month = getDate.getMonth()+1;
+  let date = getDate.getDate();
+  const [todayDate, setDate] = useState(year + '.' + month + '.' + date)
+  const [todo, setTodo] = useState('');
+  const [details, setDetails] = useState('');
+  const [selectTime, setTime] = useState('');
+  const [error, setError] = useState('')
+  const pattern = /(^\d{4}).([1-9]|1[0-2]).([1-9]|[12][0-9]|3[01])$/
+
+  const time = [
+    {num:1},{num:2},{num:3},{num:4},{num:5},{num:6},{num:7},{num:8},{num:9},{num:10},
+    {num:11},{num:12},{num:13},{num:14},{num:15},{num:16},{num:17},{num:18},{num:19},{num:20},{num:21},{num:22},{num:23},{num:24}
+  ]
+  let setTodoList = {
+	  setTodo: todo,
+	  setDetails: details,
+    setDate: todayDate,
+    setTime: selectTime,
+    setUser: loggedUser
+  }
+
+  const inputTodoChange = event => {
+    setTodo(event.target.value)
+  }
+  const inputDetailsChange = event => {
+    setDetails(event.target.value)
+  }
+
+  const selectTimeChange = event => {
+    setTime(event.target.value)
+  }
+  const setTodoDate = event => {
+    setDate(event.target.value)
+  }
+
 
   const submit = () => {
-    if(details === ''){
-      error = "상세내용을 입력해주세요"
+    if(selectTime === "시간선택" || selectTime === ""){ 
+      setError("일정시간을 선택해주세요")
+      console.log("일정시간을 선택해주세요")
+    }
+    if(details === ""){
+      setError("상세내용을 입력해주세요")
       console.log("상세내용을 입력해주세요")
     }
     if(todo === ""){
-      error = "제목을 입력해주세요"
+      setError("제목을 입력해주세요")
       console.log("제목을 입력해주세요")
+    }if(!pattern.test(todayDate)){
+      setError("정확한 날짜를 입력해주세요")
+      console.log("정확한 날짜를 입력해주세요")
+    }
+    if(selectTime !== "시간선택" && selectTime !== "" && details !== "" && todo !== "" && pattern.test(todayDate)){
+      setError("")
+      setItem({setTodoList})
     }
   }
 
+  const timeOptions =  time.map((data, idx)=>{
+    return <option value={data.num} key={idx}>{data.num}시</option>
+  })
+
   return(
-    <div className="flex items-center flex justify-center mt-32 font-jua">
+    <div className="flex items-center flex justify-center mt-32 font-Do">
       <div className="bg-slate-50 w-96 h-80 shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col flex items-center flex justify-center">
         <div className="mb-4">
           <span>제목: </span>
-          <input type="text" className="border border-gray-500"></input>
+          <input type="text" onChange={inputTodoChange} className="border border-gray-500"></input>
         </div>
 
         <div className="mb-6">
           <span>세부내용: </span>
-          <input type="text" className="border border-gray-500"></input>
+          <input type="text" onChange={inputDetailsChange} className="border border-gray-500"></input>
         </div>
 
         <div className="mb-8">
-          <label for="start" >예정일:</label>
-          <input type="text" name="start" className="border border-gray-500 w-24"></input>
-          <select id="time">
+          <label>예정일:</label>
+          <input type="text" name="start" onChange={setTodoDate} value={todayDate} className="border border-gray-500 w-24"></input>
+          <select id="time" onChange={selectTimeChange}>
             <option value="시간선택">시간선택</option>
-            {/* {#each time as {num}}
-              <option value={num}> {num}시 </option>
-            {/each} */}
+            {timeOptions}
           </select>
         </div>
-        <div>
+        <div className="text-red-500">
           <AddTodoError error = {error}/>
         </div>
         <div>
