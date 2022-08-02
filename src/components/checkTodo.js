b3c8bd0f895aed22bbe94aeb74c951c008c23cbb
Kim sanggyung <ksang0906@gmail.com>
2022-08-02 17:54:36 +0900

    모든일정 확인 컴포넌트

--- src/components/checkTodo.js
+++ src/components/checkTodo.js
@@ -1,9 +1,58 @@
-function CheckTodo(){
+import { useEffect, useState } from "react";
+import {getItem, deleteTodo, } from "../context/indexed"
+
+function CheckTodo({loggedUser, setTodoState, setEditTodoState, setCheckTodoState, setTargetID}){
+  let [getList, setGetList] = useState(null);
+
+  useEffect(() => {
+
+    const cancel = (id) => {
+      deleteTodo(id);
+      setTodoState(false)
+    }
+
+    const editMode = (id) =>{
+      setEditTodoState(true)
+      setCheckTodoState(false)
+      setTargetID(id)
+    }
+
+    const callItem = () => {
+      getItem().then(data => {
+        let getData = data;
+        let checkID = getData.find(value => value.setTodoList.setUser === loggedUser);
+        let checkedUser;
+        if(checkID){
+          checkedUser = checkID.setTodoList.setUser;
+        }
+        if(getData!== null && getData.length > 0 && checkedUser !== null && checkID !== null && loggedUser === checkedUser){
+          setGetList(getData.map(({setTodoList, id}, index) => {
+            if(setTodoList.setUser === loggedUser){
+              return(
+                <div className="mb-2 pl-4" key={id}>
+                  <div onClick={()=>editMode(id)} className="float-left">
+                    {index+1}. 일자:{setTodoList.setDate} 제목:{setTodoList.setTodo} 내용:{setTodoList.setDetails}
+                  </div>
+                  <button className="text-red-500" onClick={()=> {cancel(id)}}>X</button>
+                </div>
+              )
+            }return false;     
+          })
+        )
+        }else{
+          return(
+          setGetList(<span className="pl-4">일정이 없습니다.</span>)
+          )
+        }
+      })
+    }
+    callItem()
+  },[]);
 
   return(
-    <>
-      <div>test</div>
-    </>
+    <div className="font-Do underline bg-sky-100 mt-32 h-full text-2xl">
+      {getList}
+    </div>
   )
 }
 
