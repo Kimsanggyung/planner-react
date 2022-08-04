73409323a4fe4eb5d5523ce9426938fd34efddbf
Kim sanggyung <ksang0906@gmail.com>
2022-08-04 13:31:40 +0900

    props 넘겨주기

--- src/contents/calendar.js
+++ src/contents/calendar.js
@@ -1,13 +1,13 @@
 import Daily from "../components/daily";
 import Weekly from "../components/weekly";
 import Monthly from "../components/monthly";
-function Calendar({loginState, dailyState, weeklyState, monthlyState}){
+function Calendar({targetID, setTargetID, setAddDate, loggedUser, date, setDate, loginState, dailyState, weeklyState, monthlyState, setTodoState,setCheckDetailState, setSelectedTime,setAddTodoState }){
   let selectedCalendar = null;
   if(loginState && dailyState === true){
-    selectedCalendar = <Daily/>
+    selectedCalendar = <Daily setSelectedTime={setSelectedTime} loggedUser={loggedUser} setTodoState={setTodoState} setAddTodoState={setAddTodoState} date={date} setDate={setDate} setTargetID={setTargetID} setCheckDetailState={setCheckDetailState}/>
   }
   else if(loginState && weeklyState === true){
-    selectedCalendar =  <Weekly/>
+    selectedCalendar =  <Weekly setAddTodoState={setAddTodoState} setSelectedTime={setSelectedTime} loggedUser={loggedUser} setTodoState={setTodoState} date={date} setDate={setDate} setTargetID={setTargetID} setCheckDetailState={setCheckDetailState} setAddDate={setAddDate}/>
   }
   else if(loginState && monthlyState === true){
     selectedCalendar = <Monthly/>
--- src/contents/todo.js
+++ src/contents/todo.js
@@ -1,16 +1,19 @@
 import AddTodo from "../components/addTodo";
 import CheckTodo from "../components/checkTodo";
 import EditTodo from "../components/editTodo";
+import CheckDetail from "../components/checkDetail";
 
-function Todo({editTodoState, setEditTodoState, targetID, setTargetID, loginState, addTodoState, checkTodoState, loggedUser, setCheckTodoState, setTodoState}){
+function Todo({selectedTime,editTodoState, addDate, setSelectedTime, setEditTodoState, targetID, setTargetID, loginState, addTodoState, checkTodoState, loggedUser, setCheckTodoState, setTodoState, checkDetailState ,setCheckDetailState}){
   let selectedTodo = null;
-  console.log(editTodoState)
+  console.log(checkDetailState)
   if(loginState && addTodoState){
-    selectedTodo = <AddTodo loggedUser={loggedUser} setTodoState={setTodoState}/>
+    selectedTodo = <AddTodo selectedTime={selectedTime} setSelectedTime={setSelectedTime} loggedUser={loggedUser} setTodoState={setTodoState} addDate={addDate}/>
   }else if(loginState && checkTodoState){
-    selectedTodo =  <CheckTodo loggedUser={loggedUser} setEditTodoState={setEditTodoState} setTodoState={setTodoState} setTargetID={setTargetID} setCheckTodoState={setCheckTodoState}/>
+    selectedTodo =  <CheckTodo loggedUser={loggedUser} setEditTodoState={setEditTodoState} setTodoState={setTodoState} setTargetID={setTargetID} setCheckTodoState={setCheckTodoState} setCheckDetailState={setCheckDetailState}/>
   }else if(loginState && editTodoState){
     selectedTodo = <EditTodo targetID={targetID} loggedUser={loggedUser} setTodoState={setTodoState}/>
+  }else if(loginState && checkDetailState){
+    selectedTodo = <CheckDetail setEditTodoState={setEditTodoState} targetID={targetID} setTargetID={setTargetID} setTodoState={setTodoState}  setCheckDetailState={setCheckDetailState}/>
   }
   
 
