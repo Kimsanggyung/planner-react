8cf575d160d1fda14b1b3c27dbd346b3c06df22c
Kim sanggyung <ksang0906@gmail.com>
2022-08-07 01:08:30 +0900

    예외처리

--- src/components/checkTodo.js
+++ src/components/checkTodo.js
@@ -19,34 +19,49 @@ function CheckTodo({loggedUser, setTodoState, setCheckTodoState, setTargetID, se
 
     const callItem = () => {
       getItem().then(data => {
+
         let getData = data;
-        let checkID = getData.find(value => value.setTodoList.setUser === loggedUser);
         let checkedUser;
-        if(checkID){
-          checkedUser = checkID.setTodoList.setUser;
+
+        const result = data.find(({setTodoList})=>{
+          if (!setTodoList) return false;
+          const {setUser} = setTodoList;
+          return (setUser === loggedUser)
+        });
+
+        if(result){
+          checkedUser = result.setTodoList.setUser;
         }
-        if(getData!== null && getData.length > 0 && checkedUser !== null && checkID !== null && loggedUser === checkedUser){
-          setGetList(getData.map(({setTodoList, id}, index) => {
-            if(setTodoList.setUser === loggedUser){
-              return(
-                <div className="mb-2 pl-4" key={id}>
-                  <div onClick={()=>checkDetail(id)} className="float-left">
-                    {index+1}. 일자:{setTodoList.setDate} 제목:{setTodoList.setTodo} 내용:{setTodoList.setDetails}
-                  </div>
-                  <button className="text-red-500" onClick={()=> {cancel(id)}}>X</button>
-                </div>
-              )
-            }return false;     
-          })
-        )
+
+        console.log(checkedUser)
+        console.log(loggedUser)
+
+        if(getData!== null && getData.length > 0 && checkedUser !== undefined && result && loggedUser === checkedUser){
+          setGetList(
+            getData.map(({setTodoList, id}, index) => {
+              if(setTodoList){
+                if(setTodoList.setUser === loggedUser){
+                  return(
+                    <div className="mb-2 pl-4" key={id}>
+                      <div onClick={()=>checkDetail(id)} className="float-left">
+                        {index+1}. 일자:{setTodoList.setDate} 제목:{setTodoList.setTodo} 내용:{setTodoList.setDetails}
+                      </div>
+                      <button className="text-red-500" onClick={()=> {cancel(id)}}>X</button>
+                    </div>
+                  )
+                } 
+              }return true;     
+            })
+          )
         }else{
           return(
-          setGetList(<span className="pl-4">일정이 없습니다.</span>)
+            setGetList(<span className="pl-4">일정이 없습니다.</span>)
           )
         }
       })
     }
     callItem()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[]);
 
   return(
