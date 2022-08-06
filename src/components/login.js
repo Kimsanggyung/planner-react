040cf9fdfc504cb59cc365e4762d4f6503ff4fd0
Kim sanggyung <ksang0906@gmail.com>
2022-08-06 14:50:13 +0900

    indexedDB사용

--- src/components/login.js
+++ src/components/login.js
@@ -1,37 +1,97 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import CryptoJS from "crypto-js";
 import LoginError from '../parts/loginError'
+import { OuserData } from "../context/userData"
+import { getItem } from "../context/indexed"
 
 function Login({setSignUpState, loginState, setLoginState, setLoggedUser}){
   const [inputID, setInputID] = useState('');
   const [inputPWD, setInputPWD] = useState('');
-  const [error, setError] = useState('')
+  const [error, setError] = useState('');
+  const [hashPwd, setHashPwd] =useState('');
+  const saltKey = 'nuguseyo'
 
-  const userData = [
-    {id: 'a', pwd: '123'},
-    {id: 'b', pwd: '1234'}
-  ];
+  useEffect(()=>{
 
-  const checkUser = () => {
-		const findUser = userData.find(user => user.id === inputID && user.pwd === inputPWD)
-    if(findUser){
-      console.log("성공")
-      setLoggedUser(inputID)
-      setLoginState(!loginState)
-    }
-    else{
-      setError("아이디 또는 비밀번호를 확인해주세요")
-      console.log("틀림")
-    }
-    if(inputPWD === ''){
-      setError("비밀번호를 입력해주세요")
-      console.log("비밀번호를 입력해주세요")
-    }
-    if(inputID === ''){
-      setError("아이디를 입력해주세요")
-      console.log("아이디를 입력해주세요")
-    }
+    setHashPwd(CryptoJS.MD5(inputPWD+saltKey).toString());
+    console.log(inputID) 
+    console.log(inputPWD)
+    console.log(hashPwd)
+
+  },[inputID, inputPWD]);
+  useEffect(()=>{
+    console.log(hashPwd)
+  },[hashPwd])
+
+  const checkUser = (id, pwd) => {
+		const findUser = OuserData.find(user => user.id === id && user.pwd === pwd)
 		return findUser;
 	}
+ 
+  const login = () => {
+    
+    getItem().then(data => {
+      console.log(data.length)
+      const indexedUser = data.find(({userData})=>{
+        return userData;
+      });
+      console.log(indexedUser) 
+      if(data.length > 0){
+        const checkUserAll = data.find(({userData})=>{
+          if(userData){
+            console.log(userData)
+            const findIndexedUser = userData.userId === inputID && userData.userPwd === hashPwd;
+            console.log(userData.userPwd)
+            console.log(hashPwd)
+            // console.log(userData.userPwd === hashPwd)
+            console.log(findIndexedUser)
+            const findUser = checkUser(inputID, inputPWD);
+            return findIndexedUser || findUser;
+          }else{
+            return false;
+          }
+        }); 
+        if(inputPWD === ""){
+          setError("비밀번호를 입력해주세요");
+          console.log("비밀번호를 입력해주세요")
+        }
+        if(inputID === ""){
+          setError("아이디를 입력해주세요")
+          console.log("아이디를 입력해주세요")
+        }
+        if(inputID !== "" && inputPWD !== ""){
+          if(checkUserAll){
+            setLoggedUser(inputID)
+            setLoginState(!loginState)
+          }else{
+            setError("아이디 또는 비밀번호를 확인해주세요")
+            console.log("틀림")
+          }
+        }
+      }if(data.length === 0 || indexedUser === undefined){
+        const findUser = checkUser(inputID, inputPWD);
+        if(inputPWD === ""){
+          setError("비밀번호를 입력해주세요");
+          console.log("비밀번호를 입력해주세요")
+        }
+        if(inputID === ""){
+          setError("아이디를 입력해주세요")
+          console.log("아이디를 입력해주세요")
+        }
+        if(inputID !== "" && inputPWD !== ""){
+          if(findUser){
+            setLoggedUser(inputID)
+            setLoginState(!loginState)
+          }else{
+            setError("아이디 또는 비밀번호를 확인해주세요")
+            console.log("틀림")
+          }
+        }
+      }
+    })
+  }
+
+
 
   const inputIdChange = event => {
     setInputID(event.target.value)
@@ -73,7 +133,7 @@ function Login({setSignUpState, loginState, setLoginState, setLoggedUser}){
           </div>
 
           <div className="flex items-center justify-between">
-            <button type="submit" onClick={checkUser} className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">로그인</button>
+            <button type="submit" onClick={login} className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">로그인</button>
           </div>
           <div className="flex items-center justify-between">
             <button onClick={signUp} className="text-blue-600 mt-4 underline underline-offset-2">회원가입</button>
@@ -85,4 +145,4 @@ function Login({setSignUpState, loginState, setLoginState, setLoggedUser}){
   }
 }
 
-export default Login
+export default Login;
--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,11 +1,51 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import SignUpError from "../parts/signUpError"
+import { getItem, setItem } from "../context/indexed"
+import CryptoJS from "crypto-js";
+import { OuserData } from "../context/userData"
 
 function SignUp({setSignUpState}){
   const [inputID, setInputID] = useState("");
   const [inputPWD, setInputPWD] = useState("");
   const [checkPWD, setCheckPWD] = useState("");
   const [error, setError] = useState("")
+  const [checkState, setCheckState] = useState(false);
+  const [userData, setUserData] = useState(OuserData)
+  const [hashPwd, setHashPwd] = useState(null);
+  const [checked, setChecked] = useState('');
+  const password = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+  let saltKey = "nuguseyo"
+
+  useEffect(()=>{
+
+    const test = 'qwerqwer12'
+    setHashPwd(CryptoJS.MD5(inputPWD+saltKey).toString());
+    console.log(CryptoJS.MD5(test+saltKey).toString())
+
+    setUserData({
+      userId: inputID,
+      userPwd: hashPwd
+    })
+    console.log(inputID)
+    console.log(inputPWD)
+    console.log(hashPwd)
+    console.log(userData)
+  }, [inputID,hashPwd])
+
+  useEffect(()=>{
+    
+    setHashPwd(CryptoJS.MD5(inputPWD+saltKey).toString());
+
+    console.log(hashPwd)
+  
+  }, [inputPWD])
+
+  
+
+  const checkUser = (id) => {
+		const findUser = OuserData.find(user => user.id === id)
+		return findUser;
+	}
 
   const inputIdChange = event => {
     setInputID(event.target.value);
@@ -20,30 +60,81 @@ function SignUp({setSignUpState}){
   };
 
   const submit = () => {
+    if(checkState === false){
+      setError("아이디 중복확인을 해주세요.");
+    }
+    if(inputID === null){
+      setError("아이디를 입력해 주세요.");
+    }
+    if(!password.test(inputPWD)){
+      setError("비밀변호는 최소 8 자, 최소 하나의 문자 및 하나의 숫자를 포함하고 있어야합니다");
+    }
+    if(checkPWD === null){
+      setError("비밀번호를 한번더 입력해주세요.");
+    }
     if(inputPWD !== checkPWD){
       setError("입력하신 비밀번호가 다릅니다.")
-    };
-    if(checkPWD === ""){
-      setError("비밀번호를 한번더 입력해주세요.");
-    };
-    if(inputPWD === ""){
-      setError("비밀번호를 입력해주세요.");
+      console.log("error")
     }
-    if(inputID === ""){
-      setError("아이디를 입력해주세요");
-    };
-    if(inputID !== "" && inputPWD !== "" && checkPWD !== "" && checkPWD === inputPWD){
-      console.log('성공');
-    };
-  };
+    if(inputID !== null && inputPWD !== null && inputPWD !== null && checkPWD === inputPWD && password.test(inputPWD) && checkState === true){
+      setItem({userData})
+      console.log('성공')
+      setSignUpState(false);
+    }
+  }
 
   const cancel = () => {
     setSignUpState(false)
   }
 
+  const checkId = () => {
+    getItem().then(data => {
+      const indexedUser = data.find(({userData})=>{
+        return userData;
+      });
+      if(data.length > 0){
+        // 이미 사용중인 아이디인지 체크
+        const checkSameId = data.find(({userData}) => {
+          if(userData){
+            const checkInputId = userData.userId === inputID
+            const findId = checkUser(inputID);
+            return checkInputId || findId
+          }else{
+            return false
+          }
+        });
+
+        if(checkSameId){
+          setError("이미사용되고있는 아이디입니다.");
+          console.log("이미사용되고있는 아이디입니다.")
+          setChecked('')
+          setCheckState(false)      
+        } else {
+          setError("");
+          setChecked("사용가능한 아이디입니다.")
+          setCheckState(true);
+        }
+      }
+      if(data.length === 0 || indexedUser ===undefined){
+        const findId = checkUser(inputID);
+        if(findId){
+          setError("이미사용되고있는 아이디입니다.");
+          console.log("이미사용되고있는 아이디입니다.");
+          setChecked('')
+          setCheckState(false);
+        }else{
+          setError("");
+          setChecked("사용가능한 아이디입니다.")
+          setCheckState(true);
+        }
+      }
+    })
+  }
+
+
   return(
     <div className="flex items-center flex justify-center mt-48 font-Do">
-      <div className="bg-slate-50 w-96 h-80 shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col flex items-center flex justify-center">
+      <div className="bg-slate-50 w-96 h-full shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col flex items-center flex justify-center">
         <div className="mb-4">
           <input 
             type="text"
@@ -52,6 +143,8 @@ function SignUp({setSignUpState}){
             placeholder="아이디를 입력해주세요."
             className="form-control block px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
           />
+          <button onClick={checkId}>중복확인</button>
+          <div>{checked}</div>
         </div>
 
         <div className="mb-6">
@@ -78,7 +171,7 @@ function SignUp({setSignUpState}){
           <SignUpError error = {error}/>
         </div>
     
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between float-left">
           <button type="submit" onClick={submit} className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">등록</button>
         </div>
 
