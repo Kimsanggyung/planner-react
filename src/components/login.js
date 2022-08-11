215ec24cf86fbfa5607bcc646d646085f690636f
Kim sanggyung <ksang0906@gmail.com>
2022-08-11 13:14:13 +0900

    자동로그인 기능추가 localsotorage에 저장된 로그인정보가 있으면 그정보로 자동로그인

--- src/components/login.js
+++ src/components/login.js
@@ -13,15 +13,22 @@ function Login({setSignUpState, loginState, setLoginState, setLoggedUser}){
   const saltKey = 'nuguseyo'
 
   useEffect(()=>{// inputID inputPWD가 변경될 때 마다 실행
-
     setHashPwd(CryptoJS.MD5(inputPWD+saltKey).toString()); //inputPWD saltKey를 합쳐서 hash처리
-
   },[inputID, inputPWD]);
+
+  let saveID =localStorage.getItem('saveID')
+  
+  useEffect(()=>{ // 컴포넌트가 실행될 때
+    if(saveID){
+      setLoginState(true);
+      setLoggedUser(saveID);
+    }
+  },[])
 
   const checkUser = (id, pwd) => { // 원하는 유저 찾기
 		const findUser = OuserData.find(user => user.id === id && user.pwd === pwd); //OuserData에 아이디 비밀번호 입력한 아이디 비밀번호 비교
 		return findUser; // 비교한값 반환
-	}
+	};
  
   const login = () => {// 로그인 함수
     getItem().then(data => { // indexedDB에서 데이터 가져옴
@@ -51,51 +58,48 @@ function Login({setSignUpState, loginState, setLoginState, setLoggedUser}){
             setLoggedUser(inputID); // loggedUser를 inputID로 세팅
             setLoginState(true); //loginState를 true로 해서 달력이나 다른 화면을 보여줌
           }else{//조건이 맞지 않으면
-            setError("아이디 또는 비밀번호를 확인해주세요") //에러메시지 세팅
-            console.log("틀림") // 콘솔로그에 에러보여주기
+            setError("아이디 또는 비밀번호를 확인해주세요"); //에러메시지 세팅
+            console.log("틀림"); // 콘솔로그에 에러보여주기
           }
         }
       }if(data.length === 0 || indexedUser === undefined){ // data배열 갈이가 0이거나 indexedUser가 없다면
         const findUser = checkUser(inputID, inputPWD); //로그인하는 유저정보와 메모리에 있는 유저정보랑 비교
         if(inputPWD === ""){ //아이디 입력창이 비어있다면
           setError("비밀번호를 입력해주세요"); //에러메시지 세팅
-          console.log("비밀번호를 입력해주세요")  //콘솔로그에 에러보여주기
-        }
+          console.log("비밀번호를 입력해주세요"); //콘솔로그에 에러보여주기
+        };
         if(inputID === ""){ //비밀번호 입력창이 비어있다면
           setError("아이디를 입력해주세요"); //에러메시지 세팅
           console.log("아이디를 입력해주세요"); //콘솔로그에 에러보여주기
-        }
+        };
         if(inputID !== "" && inputPWD !== ""){ //입력창이 모두 비어있지않다면
           if(findUser){ //findUser가 참이면
             setLoggedUser(inputID); // loggedUser를 inputID로 세팅
             setLoginState(true); //loginState를 true로 해서 달력이나 다른 화면을 보여줌
           }else{//조건이 맞지 않으면
-            setError("아이디 또는 비밀번호를 확인해주세요") //에러메시지 세팅
-            console.log("틀림") // 콘솔로그에 에러보여주기
+            setError("아이디 또는 비밀번호를 확인해주세요"); //에러메시지 세팅
+            console.log("틀림"); // 콘솔로그에 에러보여주기
           }
         }
       }
-    })
+    });
     if(check === "checked"){
-      localStorage.setItem('saveLogin', inputID)
-    }
-  }
+      localStorage.setItem('saveID', inputID);
+    };
+  };
 
   const inputIdChange = event => { // 아이디 입력창에 입력을 하는 등 이벤트가 발생하면 setInputID
-    setInputID(event.target.value)
-
-  }
+    setInputID(event.target.value);
+  };
   const inputPwdChange = event => { // 비밀번호 입력창에 입력을 하는 등 이벤트가 발생하면 setInputPWD
-    setInputPWD(event.target.value)
-  } 
-
+    setInputPWD(event.target.value);
+  };
   const signUp = () => { // 회원가입 버튼 함수
     setSignUpState(true); // 회원 가입 화면을 보여주기 위해 signUpState를 ture로
-  }
-
+  };
   const clickCheck = () => {
-    setCheck("checked")
-  }
+    setCheck("checked");
+  };
 
   if(!loginState){
     return( 
@@ -127,7 +131,7 @@ function Login({setSignUpState, loginState, setLoginState, setLoggedUser}){
           <div className="flex items-center justify-between">
             <button type="submit" onClick={login} className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">로그인</button>
           </div>
-          <label><input onClick={clickCheck} type="checkbox" id="id"></input>아이디 저장</label>
+          <label><input onClick={clickCheck} className="mt-2" type="checkbox" id="id"></input>아이디 저장</label>
           <div className="flex items-center justify-between">
             <button onClick={signUp} className="text-blue-600 mt-4 underline underline-offset-2">회원가입</button>
           </div>
