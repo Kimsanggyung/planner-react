0df08fd7258a817f6bb569e4128d5e865859b737
Kim sanggyung <ksang0906@gmail.com>
2022-08-12 10:04:47 +0900

    베이스데이터 파일

---
+++ src/baseData/index.js
@@ -0,0 +1,4 @@
+export const time = [ {time:"하루종일"},
+{time:"1시"},{time:"2시"},{time:"3시"},{time:"4시"},{time:"5시"},{time:"6시"},{time:"7시"},{time:"8시"},{time:"9시"},{time:"10시"},{time:"11시"},{time:"12시"},
+{time:"13시"},{time:"14시"},{time:"15시"},{time:"16시"},{time:"17시"},{time:"18시"},{time:"19시"},{time:"20시"},{time:"21시"},{time:"22시"},{time:"23시"},{time:"24시"}
+];
